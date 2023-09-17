6b74c80163250f7b751c6cc9efbf705a376250bc
BDV <bdv2023@yandex.ru>
2023-09-17 22:50:19 +0400

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://2f37770e-d981-4707-891a-19876526bce6.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
